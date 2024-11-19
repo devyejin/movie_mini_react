d18dd630c84f8d26c39a2a83cf98f6c15684c8e1
yejin <yejinoh58@gmail.com>
2024-11-19 21:51:33 +0900

    [feat]로그인 기능 추가

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../store/slices/authSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
-
+  const dispatch = useDispatch(); //reducer 함수를 사용할 때 이용
+  const navigate = useNavigate();
   //로그인 클릭시, 로그인 아닐땐 로그아웃으로
   function handleLoginClick() {
-    
+    dispatch(login());
+    navigate("/");
   }
 
   return (
--- src/store/slices/authSlice.js
+++ src/store/slices/authSlice.js
@@ -1,13 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
+import reducer from "./movieGenreSlice";
 
 const initialState = {
   isAuthenticated: false,
+  user: {
+    name: "yejin",
+    id: "yejin@naver.com",
+  },
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
-});
+  reducers: {
+    //리듀서는 함수다!
 
+    login: (state, action) => {
+      state.isAuthenticated = true;
+    },
+
+    logout: (state, action) => {
+      state.isAuthenticated = false;
+    },
+  },
+});
+//reducer는 export해 줘야 외부에서 사용(setter)
+
+export const { login, logout } = authSlice.actions;
 export default authSlice.reducer;
