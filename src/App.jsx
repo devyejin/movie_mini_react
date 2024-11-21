197944465e3d2cba8538a6126117f04c41686f6c
yejin <yejinoh58@gmail.com>
2024-11-21 16:03:19 +0900

    [refact] Redux Store Provider 위치 변경
    localStorage 접근 후, store에 값 저장하기 위해서 Store Provider를 App.jsx  상위로 옮길 필요가 있었음

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router/root";
 import "./index.css";
-import { Provider } from "react-redux";
-import store from "./store/store";
+import { useDispatch } from "react-redux";
 
+import { useEffect } from "react";
+import { updateAuthState } from "./store/slices/authSlice";
+
+/**
+ * @todo 컴포넌트로 분리하기
+ */
 function App() {
+  const dispatch = useDispatch();
+
+  //1. localStorage에서 상태값을 가져오자
+  function getIsLoggedIn() {
+    let storage;
+    const key = "isLoggedIn";
+
+    try {
+      storage = window["localStorage"];
+      //해당 key값이 없이 null일 수도 있으니까 예외처리(js는 예외안나서 상관없나?)
+      return storage.getItem(key);
+    } catch (error) {}
+  }
+
+  const currentState = getIsLoggedIn();
+
+  //2. 가져온 상태값을 auth slice에 반영하자
+  // useEffect(() => dispatch(updateAuthState(currentState)), []);
+
+  console.log("App 호출");
+
   return (
     <>
-      <Provider store={store}>
-        <RouterProvider router={router}></RouterProvider>
-      </Provider>
+      <RouterProvider router={router}></RouterProvider>
     </>
   );
 }
