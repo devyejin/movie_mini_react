64c8156f077f04be4025389bd30bb25f0b69ec37
yejin <yejinoh58@gmail.com>
2024-12-01 03:42:39 +0900

    [refact] API 요청에서 api_key 전송 방식 변경 : query param -> Authorization header

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,10 @@ const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   params: {
     language: "ko",
-    api_key: import.meta.env.VITE_TMDB_API_KEY,
+  },
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+    Accept: "application/json",
   },
 });
 
